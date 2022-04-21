864fe299a181af47452b07497a0aa4c656508d74
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-04-21 12:22:06 +0500

    optimised

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
 let text = "98c 900a 1z23 24x6";
-let newArr = [];
-text.split(" ").forEach(element => {
-  newArr.push(element.split("").sort().reverse().join(""));
-});
+let newArr = text.split(" ").map((element) => element.split("").sort().reverse().join(""));
 console.log(newArr.sort().join(" "));
-
